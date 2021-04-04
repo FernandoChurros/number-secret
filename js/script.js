c0a58be28170c4af9e552949aa83313462cb1405
FernandoChurros <kappapridelgbt@gmail.com>
2021-04-03 23:13:30 -0300

    add feature validation if is number ou not

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const inputNumber = document.querySelector('#inputNumber');
 const btn = document.querySelector('#btn');
 const tentatives = document.querySelector('#tentatives');
-const resultDiv = document.querySelector('#resultDiv');
-const p = document.createElement('p');
+const popup = document.querySelector('.popup');
 let chances = 10;
+
+const h2 = document.querySelector('.title_popup');
+const paragraph = document.querySelector('.paragraph_popup');
+const btnClose = document.querySelector('.btn_close');
 
 function raffleNumber() {
    let numberSecret = parseInt(Math.random() * 101);
@@ -14,7 +17,7 @@ let numberSecret = raffleNumber();
 
 function validation() {
    if(inputNumber.value == '') {
-      console.log('Insira um número válido senhor')
+      invalid();
    } else {
       if (inputNumber.value == numberSecret) {
          win();
@@ -26,6 +29,14 @@ function validation() {
    inputNumber.value = '';
 }
 
+function invalid() {
+
+   console.log('Insira um número válido senhor');
+   h2.textContent = 'WARNING!';
+   paragraph.textContent = 'The machine does not recognize what you are typing';
+   popup.style.display = 'flex';
+}
+
 function win() {
 
    hit();
@@ -35,13 +46,11 @@ function win() {
    tentatives.textContent = chances;
 }
 
-const h2 = document.querySelector('.title_popup');
-const paragraph = document.querySelector('.paragraph_popup');
 
 function hit() {
    h2.textContent = 'You Won';
    paragraph.textContent = "My mental CPU with an IQ of 530 thousand, comes to just one conclusion, you won the machine, Congratulations"
-   document.querySelector('.popup').style.display = 'flex'
+   popup.style.display = 'flex'
 }
 
 function lose() {
@@ -59,14 +68,13 @@ function endTentatives() {
 
       h2.textContent = 'You lost';
       paragraph.textContent = 'We will raffle the secret number again';
-      document.querySelector('.popup').style.display = 'flex'
+      popup.style.display = 'flex'
 
       numberSecret = raffleNumber();
       chances = 10;
       tentatives.textContent = chances;
 }
 
-const btnClose = document.querySelector('.btn_close');
 function closePopUp() {
    document.querySelector('.popup').style.display = 'none';
 }
